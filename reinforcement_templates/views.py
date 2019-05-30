a4e0a9bec948513657382b849cff68a6851fbd21
ynjacobs <ytj64611@gamil.com>
2019-05-30 14:18:13 -0400

    using python in html to dry it

--- reinforcement_templates/views.py
+++ reinforcement_templates/views.py
@@ -1,6 +1,32 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+
+
+
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
+    email = forms.CharField(label='email', max_length=100)
+
+# def get_name(request):
+#     # if this is a POST request we need to process the form data
+#     if request.method == 'POST':
+#         # create a form instance and populate it with data from the request:
+#         form = NameForm(request.POST)
+#         # check whether it's valid:
+#         if form.is_valid():
+#             # process the data in form.cleaned_data as required
+#             # ...
+#             # redirect to a new URL:
+#             return HttpResponseRedirect('/thanks/')
+
+#     # if a GET (or any other method) we'll create a blank form
+#     else:
+#         form = NameForm()
+
+#     return render(request, 'new.html', {'form': form})
 
 def new_profile(request):
-    context = {}
+    context = {'fields': ['email', 'username', 'pin', 'website'] }
     return HttpResponse(render(request, 'profiles/new.html', context))
